a1ffcdc137c40325b1a4f153a67a124a49917a7b
Mauro Antonio dos Santos <santos.mauroantonio@gmail.com>
2023-08-25 04:07:59 -0300

    * Da aula de Responsividade sobre Cards.

--- flutter_playground/lib/home.dart
+++ flutter_playground/lib/home.dart
@@ -10,40 +10,101 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.sizeOf(context);
-
     return Scaffold(
       appBar: AppBar(
-        centerTitle: false,
         title: const Text("Flutter Playground"),
+        centerTitle: false,
         elevation: 1.0,
       ),
-      body: SizedBox(
-        width: size.width,
-        child: const Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Square(
-              color: Colors.red,
-              size: 100,
-              text: "1",
+      body: const Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          CustomCard(
+            model: CardModel(
+              name: 'Mauro A. Santos',
+              urlImage:
+                  'https://media.licdn.com/dms/image/D4D03AQHRCbj1hD5uFg/profile-displayphoto-shrink_800_800/0/1690832740659?e=1698278400&v=beta&t=Z7wLdhQXsxHdD8n0G2Tb4d6cqXKmSagEThVo2Ydf4mI',
             ),
-            Square(
-              color: Colors.yellow,
-              size: 100,
-              text: "2",
+          ),
+          CustomCard(
+            model: CardModel(
+              name: 'Julio Macedo',
+              urlImage:
+                  'https://media.licdn.com/dms/image/C4D03AQGZ3IJO5J0LeA/profile-displayphoto-shrink_800_800/0/1575557698635?e=1698278400&v=beta&t=pE5lBK8deRqZIhBv_Wl3cghTAUtVE2qQ9vlyoRFk7Ok',
             ),
-            Square(
-              color: Colors.green,
-              size: 100,
-              text: "3",
+          ),
+          CustomCard(
+            model: CardModel(
+              name: 'Vanessa Santos',
+              urlImage:
+                  'https://media.licdn.com/dms/image/D4D03AQGW0SmgXMefEA/profile-displayphoto-shrink_800_800/0/1666653051098?e=1698278400&v=beta&t=tVkPk0ZASTtUdw8r77TGzi8I8ZB6-WnD30mo971yFyg',
             ),
-          ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomCard extends StatelessWidget {
+  final CardModel model;
+  final double height;
+
+  const CustomCard({
+    super.key,
+    required this.model,
+    this.height = 150.0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20.0),
+      child: SizedBox(
+        height: height,
+        child: Card(
+          child: Stack(
+            children: [
+              SizedBox(
+                width: double.infinity,
+                child: Image.network(
+                  fit: BoxFit.cover,
+                  model.urlImage,
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  height: height * 0.40,
+                  color: Colors.black.withOpacity(0.50),
+                  width: double.infinity,
+                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(model.name),
+                      const Text('Flutterando Masterclass'),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+class CardModel {
+  final String name;
+  final String urlImage;
+
+  const CardModel({
+    required this.name,
+    required this.urlImage,
+  });
 }
 
 class Square extends StatelessWidget {
